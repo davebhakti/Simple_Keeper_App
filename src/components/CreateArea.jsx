de491bc3fff63b78ed0634f1a888c377eaf1b4de
davebhakti <154651813+davebhakti@users.noreply.github.com>
2025-08-04 08:07:32 +0000

    Added the handleChange function to add new items to the list

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
+        <input name="title" onChange={handleChange} placeholder="Title" />
         <textarea name="content" placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
